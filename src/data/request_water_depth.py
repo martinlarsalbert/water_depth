a4c5a725671ec2bc4b2c1a9810af34977eb82c69
martinlarsalbert <marale@kth.se>
2021-05-15 17:48:19 +0200

    first attempt with my own requests...

---
+++ src/data/request_water_depth.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import requests
+import xml.etree.ElementTree as ET
+import re
+import datetime
+import os
+
+def get_data_from_response(response)->pd.Series:
+    
+    xml = response.text
+    root = ET.fromstring(xml)
+
+    df_data = pd.DataFrame()
+
+    regexp = re.compile('\{[^}]*\}')
+
+
+    for value in root.findall('{https://opendata.smhi.se/xsd/ocobs_v1.xsd}value'):
+
+        s = pd.Series()
+
+        for data in value:
+            key = regexp.sub('', string = data.tag)
+            s[key] = data.text
+
+        s.name = s.date
+
+        df_data = df_data.append(s)
+
+    df_data.drop(columns=['date'], inplace=True)
+    df_data.index = pd.to_datetime(df_data.index)
+    data_depth = df_data['value'].astype('float')
+
+    return data_depth
+
+def request(station:int=2110, url = r'https://opendata-download-ocobs.smhi.se/'):
+    response = requests.get(f'{url}api/version/latest/parameter/6/station/{station}/period/latest-day/data.xml')
+    return response
+
+def get(station:int=2110)->pd.Series:
+    response = request(station=station)
+    data_depth = get_data_from_response(response=response)
+    return data_depth
+
+def run(save_dir_path = 'data'):
+
+    if not os.path.exists(save_dir_path):
+        os.mkdir(save_dir_path)
+    
+    station = 2110  ## (Stenungsund)
+    print('request water depth')
+    data_depth = get(station=station)
+    
+    now = datetime.datetime.now()
+    file_name = f'station_{station}_{now}.csv'
+    file_path = os.path.join(save_dir_path, file_name)
+    print(f'Saving to:{file_path}')
+    data_depth.to_csv(file_path)
+
+if __name__ == '__main__':
+    run()
---
+++ tests/test_request_water_depth.py
@@ -0,0 +1,13 @@
+import src.data.request_water_depth as request_water_depth
+
+def test_request():
+    response = request_water_depth.request()
+    assert response.status_code==200
+
+def test_get():
+    data_depth = request_water_depth.get()
+
+def test_run(tmpdir):
+
+    save_dir_path = str(tmpdir)
+    request_water_depth.run(save_dir_path=save_dir_path)
